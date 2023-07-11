d63c270f7c995aed7d1a45995cc9e4ced534f127
edwyn.placencia <Edwyn.Placencia@cignium.com>
2023-07-11 11:26:46 -0500

    Condicionales

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/eplacenciatz/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(123)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// estado, texto := variables.ConviertoaTexto(123)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es", os)
+	} else {
+		fmt.Println("Esto es", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
 }

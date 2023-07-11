63ef716ab766b0a50e92321d0fd7f8bb764606c9
edwyn.placencia <Edwyn.Placencia@cignium.com>
2023-07-11 10:32:59 -0500

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eplacenciatz/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/eplacenciatz/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
